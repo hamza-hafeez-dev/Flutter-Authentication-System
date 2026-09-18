307d5282efebb32fe5c9959739466b7b003dd0e4
Muhammad Hamza Hafeez <official.hamzahafeez@gmail.com>
2026-09-19 01:59:17 +0500

    Implement Welcome Page UI with navigation

--- Welcome-Page-UI-Code .dart
+++ Welcome-Page-UI-Code .dart
@@ -1,1 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login_app/login.dart';
+import 'package:flutter_login_app/register.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fruzz digital',
+      home: MainPage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.white,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.black54,
+          primary: Colors.black54,
+        ),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.sizeOf(context).height;
+    final width = MediaQuery.sizeOf(context).width;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: Image.asset('assets/backgroundimage.png', fit: BoxFit.fill),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Center(
+                  child: SizedBox(
+                    height: height * 0.13,
+                    width: width * 0.4,
+                    child: Image.asset(
+                      'assets/logoimage.png',
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: height * 0.05),
+                SizedBox(
+                  height: height * 0.065,
+                  width: width * 0.85,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const LoginPage(),
+                          ),
+                        );
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black87,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Login',
+                        style: TextStyle(fontSize: 22, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: height * 0.02),
+                SizedBox(
+                  height: height * 0.065,
+                  width: width * 0.85,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      setState(() {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterPage(),
+                          ),
+                        );
+                      });
+                    },
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Register',
+                        style: TextStyle(fontSize: 22, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: height * 0.05),
+                Text(
+                  "Continue as a guest !",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.cyan,
+                  ),
+                ),
+                SizedBox(height: height * 0.06),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
