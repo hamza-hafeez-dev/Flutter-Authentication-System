28aea22fcfff57ae958a47e0a789d3b0adfa2ffc
Muhammad Hamza Hafeez <official.hamzahafeez@gmail.com>
2026-09-20 20:28:25 +0500

    Implement Forgot Password UI with email input

--- 4. ForgotPassword-UI-Code .dart
+++ 4. ForgotPassword-UI-Code .dart
@@ -1,1 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login_app/login.dart';
+import 'package:flutter_login_app/otpui.dart';
 
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({super.key});
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  @override
+  Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
+
+    final height = MediaQuery.sizeOf(context).height;
+    final width = MediaQuery.sizeOf(context).width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: SafeArea(
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(22.0),
+                  child: Column(
+                    crossAxisAlignment: .start,
+                    children: [
+                      Container(
+                        height: height * 0.045,
+                        width: width * 0.1,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(width: 2, color: Colors.grey),
+                        ),
+
+                        child: IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(
+                            Icons.arrow_back_ios_new,
+                            size: 15,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: height * 0.035),
+
+                      Text(
+                        'Forgot Password ?',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF1E232C),
+                        ),
+                      ),
+
+                      SizedBox(height: height * 0.015),
+
+                      Text(
+                        'Don t Worry! it occur. Please enter the email\naddress linkedwith your accounts',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                      ),
+
+                      SizedBox(height: height * 0.025),
+                      SizedBox(
+                        height: height * 0.08,
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return 'Email is Required !';
+                            }
+                            return null;
+                          },
+
+                          style: TextStyle(fontSize: 18, color: Colors.black),
+                          decoration: InputDecoration(
+                            labelText: 'Enter Your Email',
+                            labelStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 18,
+                            ),
+
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: height * 0.02),
+                      SizedBox(
+                        height: height * 0.065,
+                        child: InkWell(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (formKey.currentState!.validate()) {
+                                setState(() {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const OtpVerification(),
+                                    ),
+                                  );
+                                });
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black87,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Send Code',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: height * 0.45),
+                      Row(
+                        mainAxisAlignment: .center,
+                        children: [
+                          Spacer(),
+                          Text(
+                            'Remenber Password ?',
+                            style: TextStyle(fontSize: 15, color: Colors.black),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const LoginPage(),
+                                  ),
+                                );
+                              });
+                            },
+                            child: Text(
+                              'Login Now',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                          Spacer(),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
